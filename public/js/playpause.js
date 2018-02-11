da8d0c8993a6936a8ebee088ecb5f9002df5c471
jubby <luigi_334@hotmail.com>
2018-02-11 14:23:57 +0000

    js update

--- public/js/playpause.js
+++ public/js/playpause.js
@@ -1,26 +1,32 @@
 const wrappers = document.querySelectorAll('.wrapper');
 
 wrappers.forEach(wrapper => {
+    const myTrack = wrapper.getElementsByClassName('myTrack')[0];
+
     const playButton = wrapper.getElementsByClassName('playButton')[0];
 
-    const barSize = 120;
-    const bar = document.querySelector('#defaultBar');
-    const progressBar = document.querySelector('#progressBar');
+    const progressBar = wrapper.querySelector('.progressBar');
 
-    const currentTime = wrapper.querySelector('#current');
-    const durTime = wrapper.querySelector('#duration');
-
-    const myTrack = wrapper.getElementsByClassName('myTrack')[0];
+    const currentTime = wrapper.querySelector('.current');
+    const durTime = wrapper.querySelector('.duration');
     const duration = myTrack.duration;
 
-
-    wrapper.querySelector('#duration').innerHTML = secondsToMinutes(duration);
+    wrapper.querySelector('.duration').innerHTML = secondsToMinutes(duration);
 
     playButton.addEventListener('click', playOrPause, false);
     myTrack.ontimeupdate = function () {
         currentTime.innerHTML = secondsToMinutes(myTrack.currentTime);
 
+        let size = (myTrack.currentTime / duration * 100);
+        progressBar.value = size;
     }
+
+    progressBar.addEventListener('click', function (event) {
+        const percent = event.offsetX / event.target.offsetWidth;
+        myTrack.currentTime = percent * duration;
+        progressBar.value = percent * 100;
+
+    })
 
     function playOrPause() {
         if (!myTrack.paused && !myTrack.ended) {
@@ -30,9 +36,6 @@ wrappers.forEach(wrapper => {
         } else {
             myTrack.play();
             playButton.style.backgroundImage = 'url(/images/pause.png)';
-
-            let size = (currentTime * barSize / durTime);
-            progressBar.style.width = size + "px";
         }
     }
 
