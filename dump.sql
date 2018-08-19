1ad7cb04b0996e795107c643293db6ce53389880
jubby <luigi_334@hotmail.com>
2018-08-19 20:57:39 +0100

    Buy tracks db now showing. created function for access to db.

--- dump.sql
+++ dump.sql
@@ -1,73 +1,102 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 26, 2018 at 02:53 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
+-- Host: localhost    Database: av
+-- ------------------------------------------------------
+-- Server version	5.7.22-0ubuntu0.17.10.1
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 --
--- Database: `av`
+-- Table structure for table `products`
 --
-CREATE DATABASE IF NOT EXISTS `av` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE `av`;
 USE `av`;
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products`
+(
+  `id` int
+(11) DEFAULT NULL,
+  `trackname` text,
+  `tracklength` time DEFAULT NULL,
+  `bpm` decimal
+(10,0) DEFAULT NULL,
+  `price` decimal
+(10,0) DEFAULT NULL,
+  `genre` text,
+  `format` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `products`
+--
 
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `
+products`
+VALUES
+  (1, 'We run tings', '00:04:06', 140, 2, 'Bassline', 'MP3'),
+  (2, 'Danger', '00:03:54', 140, 2, 'Bassline', 'MP3'),
+  (3, 'Human no machine', '00:04:06', 140, 2, 'Bassline', 'MP3'),
+  (4, 'We run tings', '00:04:06', 140, 2, 'Bassline', 'WAV'),
+  (5, 'Danger', '00:03:54', 140, 2, 'Bassline', 'WAV'),
+  (6, 'Human no machine', '00:04:06', 140, 2, 'Bassline', 'WAV'),
+  (7, 'We run tings', '00:04:06', 140, 2, 'Bassline', 'AAC'),
+  (8, 'Danger', '00:03:54', 140, 2, 'Bassline', 'AAC'),
+  (9, 'Human no machine', '00:04:06', 140, 2, 'Bassline', 'AAC');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
 --
 -- Table structure for table `users`
 --
-
-CREATE TABLE `users` (
-  `id` int(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users`
+(
+  `id` int
+(255) NOT NULL AUTO_INCREMENT,
+  `username` varchar
+(255) NOT NULL,
+  `password` varchar
+(255) NOT NULL,
+  `email` varchar
+(255) NOT NULL,
+  PRIMARY KEY
+(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'testuser', 'password', 'test@test.com'),
-(3, 'jubby', '12345', 'jubby@jubby.com'),
-(6, 'test', 'test', 'test@test.com'),
-(7, 'jubby', 'jubby', 'jubby@jubby.com');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `
+users`
+VALUES
+  (12, 'test', '$2y$10$OC0L0jTl0CNx.ELYGS8iNuAM8HtrWbP0rPIVfEixfi/EfHleuou2e', 'test@test.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-23 12:58:35
--- src/Controller/BuyController.php
+++ src/Controller/BuyController.php
@@ -12,6 +12,33 @@ class BuyController
 
     public function get($request, $response, $args)
     {
-        return $this->view->render($response, 'buy.html.twig');
+        $products = \Jubby\Model\Product::all();
+
+        return $this->view->render($response, 'buy.html.twig', [
+            'products' => $products
+        ]);
     }
+        
 }
+// below is the public function above in short done by elequent. 
+// class Product {
+//     public $tracname;
+//     public $duration;
+//     public $type;
+
+//     public function __construct($trackname, $duration, $type)
+//     {
+//         $this->trackname = $trackname;
+//         $this->duration = $duration;
+//         $this->type = $type;
+//     }
+// }
+
+// $products = [];
+
+// foreach ($db as $row) {
+//     $products[] = new Product($row['trackname'], $row['duration'], $row['type']);
+// }
+
+// $procuts = new product();
+// $procuts->trackname;
